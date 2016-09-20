eef46a22ce63a938c5bc8d1497e89cffa9d67fdd
Francois <francois@n-dream.com>
2016-09-20 15:45:57 +0200

    Import Log

--- src/android/UrlOverride.java
+++ src/android/UrlOverride.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 public class UrlOverride extends CordovaPlugin {
 
   private static final String TAG = "UrlOverride";
