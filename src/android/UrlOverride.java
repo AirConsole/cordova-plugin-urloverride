2869db9d981303ea73563651faa0451854318778
Francois <francois@n-dream.com>
2016-09-20 15:32:34 +0200

    Updated plugin with cordova guide methods

--- src/android/UrlOverride.java
+++ src/android/UrlOverride.java
@@ -1,15 +1,20 @@
-package org.apache.cordova.urloverride;
+package org.apache.cordova.plugin;
 
-import android.app.Activity;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UrlOverride extends CordovaPlugin {
 
   private static final String TAG = "UrlOverride";
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+      super.initialize(cordova, webView);
+  }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
