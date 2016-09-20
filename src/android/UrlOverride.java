f3d19b158760211aedf2e8b9d68824afa17ef94c
Francois <francois@n-dream.com>
2016-09-20 16:21:08 +0200

    StartActivity with cordova ref

--- src/android/UrlOverride.java
+++ src/android/UrlOverride.java
@@ -2,6 +2,7 @@ package org.apache.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,7 +14,16 @@ import android.content.Intent;
 
 public class UrlOverride extends CordovaPlugin {
 
+  // private CordovaInterface cordova;
   private static final String TAG = "UrlOverride";
+
+  // @Override
+  // public void init(CordovaInterface cordova) {
+  //     if (this.cordova != null) {
+  //         throw new IllegalStateException();
+  //     }
+  //     this.cordova = cordova;
+  // }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -26,7 +36,7 @@ public class UrlOverride extends CordovaPlugin {
     Log.d(TAG, "onOverrideUrlLoading: " + url);
     if (url.length() != 0 && !url.startsWith("file://")) {
       Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-      startActivity(i);
+      this.cordova.getActivity().startActivity(i);
       return true;
     }
     return false;
