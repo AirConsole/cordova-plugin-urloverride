f36ac00c1627edb5cddd92c184528bac9c887bba
Francois <francois@n-dream.com>
2016-09-21 12:56:21 +0200

    Implementation for iOS

--- src/ios/CDVUrlOverride.m
+++ src/ios/CDVUrlOverride.m
@@ -3,33 +3,21 @@
 
 @implementation CDVUrlOverride
 
-// - (BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType {
-//     NSString* requestedUrl = [[request URL] absoluteString];
-//     NSLog(@"shouldStartLoadWithRequest %@", requestedUrl);
-
-//     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
-//       // NSString* jsMethodTemplate = @"linkHijackiOS.linkIntercepted('+++');";
-//       // NSString* jsMethod = [jsMethodTemplate stringByReplacingOccurrencesOfString:@"+++" withString:requestedUrl];
-//       // [self.webView stringByEvaluatingJavaScriptFromString:jsMethod];
-//       return NO;
-//     }
-//     return [super webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
-// }
-
-- (BOOL)shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
-{
+- (BOOL)shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType {
     NSURL *url = [request URL];
-
     NSLog(@"shouldStartLoadWithRequest %@", url);
 
     if (![[url scheme] isEqualToString:@"file"]) {
-        if ([[UIApplication sharedApplication] canOpenURL:url]) {
-            [[UIApplication sharedApplication] openURL:url];
-            return YES;
+        if ([url isEqual:request.mainDocumentURL]) {
+            NSLog(@"CDVUrlOverride return NO");
+            if ([[UIApplication sharedApplication] canOpenURL:url]) {
+                   [[UIApplication sharedApplication] openURL:url];
+                return NO;
+            }
         }
     }
 
-    return NO;
+    return YES;
 }
 
 @end
